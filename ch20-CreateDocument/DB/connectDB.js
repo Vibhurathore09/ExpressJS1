5b9bf33800746a7a32d510e901a8f6fa6d101f23
Vibhu <vibhurathoreshic@gmail.com>
2023-04-04 18:09:03 +0530

    Create or Insert Document

---
+++ ch20-CreateDocument/DB/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async (DATABASE_URL) =>{
+    try {
+        const DB_OPTIONS = {
+            dbName : 'schoolDb'
+        }
+        // await mongoose.connect(DATABASE_URL);
+        await mongoose.connect(DATABASE_URL , DB_OPTIONS);
+        console.log("Database connected");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default connectDB;
---
+++ ch20-CreateDocument/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "./models/student.js";
+import connectDB from "./DB/connectDB.js";
+const app = express();
+const port = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/";
+
+// Database connection
+connectDB(DATABASE_URL);
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
