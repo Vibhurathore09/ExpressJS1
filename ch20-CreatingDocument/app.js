b73e357fbb98bd770e810f31ea9494c4c393dfc1
Vibhu <vibhurathoreshic@gmail.com>
2023-04-04 22:23:17 +0530

    Reterive Data from Document

--- ch20-CreatingDocument/app.js
+++ ch20-CreatingDocument/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import './models/student.js'
 import connectDB from "./DB/connectDB.js";
-import {getAllDoc , getAllDocSpecificField} from "./models/student.js";
+import {getAllDoc , getAllDocSpecificField , getSingleDoc , getSingleDocSpecificField ,getDocByField , getDocByFieldSpecificField , getLimitedDoc , getSkipDoc , getDocCount, getSortedDoc, compDoc, logDoc} from "./models/student.js";
 const app = express();
 const port = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/";
@@ -10,7 +10,18 @@ const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/";
 connectDB(DATABASE_URL);
 
 // getAllDoc();
-getAllDocSpecificField();
+// getAllDocSpecificField();
+// getSingleDoc();
+// getSingleDocSpecificField();
+// getDocByField();
+// getDocByFieldSpecificField();
+// getLimitedDoc();
+// getSkipDoc(); 
+// getDocCount();
+// getSortedDoc();
+// compDoc();
+logDoc();
+
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
--- ch20-CreatingDocument/models/student.js
+++ ch20-CreatingDocument/models/student.js
@@ -22,7 +22,7 @@ const studentSchema = new mongoose.Schema({
 const studentModel = mongoose.model("student", studentSchema);
 
 // Retriving document
-const getAllDoc = async () =>{
+const getAllDoc = async () => {
   const result = await studentModel.find();
   // console.log(result);
   // result.forEach((doc) => {
@@ -38,16 +38,89 @@ const getAllDoc = async () =>{
   //     doc.comments[0].publish,
   //   );
   // });
-}
-
+};
 
 // Retriving All Document With Specific Fields
-const getAllDocSpecificField = async () =>{
+const getAllDocSpecificField = async () => {
   // const result = await studentModel.find().select('name age'); Include
   // const result = await studentModel.find().select(['name' , 'age']); //Include
-  const result = await studentModel.find().select(['-name' , '-age']); //Exclude both ways [] , '' also works
-  console.log(result);
+  const result = await studentModel.find().select(["-name", "-age"]); //Exclude both ways [] , '' also works
+  // console.log(result);
+};
 
+// Retrieve Single Document
+const getSingleDoc = async () => {
+  const result = await studentModel.findById("642c40156eb2e58d98ab47f0");
+  console.log(result);
+  // console.log(result._id.getTimestamp())
+};
+
+// Reterive Single Document with specific fields
+const getSingleDocSpecificField = async () => {
+  const result = await studentModel.findById("642c40156eb2e58d98ab47f0").select("name age");
+  console.log(result);
+  // console.log(result._id.getTimestamp())
+};
+
+// Reterive Document by Fields
+const getDocByField = async () => {
+  const result = await studentModel.find({name : "Vibhu"});
+  // console.log(result);
+};
+
+// Reterive Document by Field with Specific Field
+
+const getDocByFieldSpecificField = async () => {
+  const result = await studentModel.find({name : "Vibhu"}).select('name age');
+  // console.log(result);
+};
+
+//  reterive Limited Document
+const getLimitedDoc = async () => {
+  const result = await studentModel.find().limit(2);
+  // console.log(result);
+};
+
+// Reterieve Skip Document
+const getSkipDoc = async () =>{
+  const result = await studentModel.find().skip(1);
+  console.log(result);
 }
 
-export  {getAllDoc , getAllDocSpecificField};
+// Count Documents
+const getDocCount = async () =>{
+  const result = await studentModel.find().count();
+  console.log(result);
+
+};
+// Count Documents
+const getSortedDoc = async () =>{
+  // const result = await studentModel.find().sort({age : 1}); //Ascending
+  const result = await studentModel.find().sort({age : -1}); //Descending
+
+  console.log(result);
+
+};
+
+// Comparison Query Operator
+const compDoc = async()=>{
+  // const result = await studentModel.find({age : {$gt : 30}}); //Age greater than 25
+  // const result = await studentModel.find({age : {$gte : 30}}); //Age greater equal than to 25
+  // const result = await studentModel.find({age : {$lte : 30}}); //Age lower than 25
+  // const result = await studentModel.find({age : {$le : 30}}); //Age lower than equal to 25
+  // const result = /await studentModel.find({age : {$ne: 33}}) // Age not equal too
+  // const result = await studentModel.find({age : {$in: [20 ,33]}}) // Age not equal too
+  const result = await studentModel.find({age : {$nin: [20 ,33]}}) // Age not equal too
+  console.log(result);
+}
+
+// Logical Query Operator
+const logDoc = async () =>{
+  // const result = await studentModel.find({$and : [{age : 27} , {fees : 6500.4}]})
+  const result = await studentModel.find({$or : [{age : 33} , {fees : 6500.4}]})
+  console.log(result);
+}
+
+
+export { getAllDoc, getAllDocSpecificField, getSingleDoc , getSingleDocSpecificField ,getDocByField ,getDocByFieldSpecificField ,
+getLimitedDoc , getSkipDoc, getDocCount , getSortedDoc,compDoc , logDoc};
