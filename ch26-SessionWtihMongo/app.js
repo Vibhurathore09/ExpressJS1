0a22af4285aecd81a3794f60cb9801e9303fd5bc
Vibhu <vibhurathoreshic@gmail.com>
2023-04-11 23:12:01 +0530

    Complete Sesssion Mongo

--- ch26-SessionWtihMongo/app.js
+++ ch26-SessionWtihMongo/app.js
@@ -1,8 +1,22 @@
 import express from "express";
 import session from "express-session";
 import web from "./routes/web.js";
+import connectDB from "./db/connectDB.js";
+import MongoStore from "connect-mongo";
 const app = express();
 const port = process.nextTick.PORT || 3000;
+const Database_URl = process.env.Database_URl || "mongodb://127.0.0.1:27017/";
+// Database Connection
+connectDB(Database_URl);
+
+// MongoDB Session
+const sessionStorage = MongoStore.create({
+  mongoUrl: Database_URl,
+  dbName: "schoolDb",
+  collectionName: "sessions",
+  ttl: 14 * 24 * 60 * 60, //14 days
+  autoRemove: "native",
+});
 
 // Session
 app.use(
@@ -11,7 +25,8 @@ app.use(
     secret: "iamkey",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 10000 },
+    cookie: { maxAge: 100000 },
+    store: sessionStorage,
   })
 );
 
