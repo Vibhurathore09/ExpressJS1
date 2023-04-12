cadeb25f57bb2fc828a77c5464df32bdd5502a70
Vibhu <vibhurathoreshic@gmail.com>
2023-04-12 20:23:36 +0530

    Complete Sesssion Mongo

---
+++ ch27-RegisterLogin/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+// import "./models/student.js";
+import connectDB from "./db/connectDB.js";
+// import createDoc from "./models/student.js";
+const app = express();
+const port = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/";
+
+
+// Database connection
+connectDB(DATABASE_URL);
+
+
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
