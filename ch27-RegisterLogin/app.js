ad30a6b837fa8042d6e797888fdb6c6a6010530d
Vibhu <vibhurathoreshic@gmail.com>
2023-04-12 22:18:07 +0530

    ch27 - updated with dotenv

--- ch27-RegisterLogin/app.js
+++ ch27-RegisterLogin/app.js
@@ -1,9 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from "express";
 import web from "./routes/web.js";
 import connectDB from "./db/connectDB.js";
 const app = express();
-const port = process.env.PORT || 3000;
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/";
+const port = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 // Set view engine
 app.set("view engine", "ejs");
--- ch27-RegisterLogin/db/connectDB.js
+++ ch27-RegisterLogin/db/connectDB.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDB = async (DATABASE_URL) =>{
-    try {
-        const DB_OPTIONS = {
-            dbName : 'blogdb',
-        }
-        // await mongoose.connect(DATABASE_URL);
-        await mongoose.connect(DATABASE_URL , DB_OPTIONS);
-        console.log("Database connected");
-    } catch (err) {
-        console.log(err);
-    }
-}
+const connectDB = async (DATABASE_URL) => {
+  try {
+    const DB_OPTIONS = {
+      dbName: process.env.dbName || "blogdb",
+    };
+    // await mongoose.connect(DATABASE_URL);
+    await mongoose.connect(DATABASE_URL, DB_OPTIONS);
+    console.log("Database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default connectDB;
